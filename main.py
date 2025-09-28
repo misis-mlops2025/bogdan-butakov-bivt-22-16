9429f8e27c789960cd54f5d5d89d120d9896603a
ButakovBI <butakovbi@gmail.com>
2025-09-28 16:43:42 +0300

    change sum function logic

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def get_sum(a: float, b: float) -> float:
-    return a + b
+def get_sum(a: float, b: float, c: float) -> float:
+    return a + b + c
 
 
 if __name__ == '__main__':
-    num1, num2 = map(float, input().split())
-    res = get_sum(num1, num2)
+    num1, num2, num3 = map(float, input().split())
+    res = get_sum(num1, num2, num3)
     print(res)
